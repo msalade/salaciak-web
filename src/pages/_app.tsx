2e37c16dd7b2b5602a03d693d5d8c1e33a699578
michal.salaciak <salaciak.michal@gmail.com>
2022-11-16 21:59:52 +0100

    fix viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width,user-scalable=yes"
         />
         <meta name="keywords" content="Michał Sałaciak" />
         <title>Michał Sałaciak</title>
